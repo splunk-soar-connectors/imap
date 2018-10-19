c2369f2d7d375e0cb80023e182822fae873d624d
gary.jeon <gjeon@splunk.com>
2018-10-19 14:23:18 -0700

    PAPP-5205 imap - remove unicode dash from the copyright info

--- imap_consts.py
+++ imap_consts.py
@@ -3,6 +3,8 @@
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
+#
+# --
 
 IMAP_JSON_USE_SSL = "use_ssl"
 
--- process_email.py
+++ process_email.py
@@ -1,8 +1,11 @@
 # File: process_email.py
+#
 # Copyright (c) 2014-2018 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
+#
+# --
 
 import email
 import tempfile
