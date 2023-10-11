862f1ca735859256e962d74b35c1980933e19a28
aparekh-crest <aparekh@splunk.com>
2023-10-11 15:18:06 +0530

    Removed unused file

--- imap_client.py
+++
@@ -1,9 +0,0 @@
-import imaplib
-from imapclient import imap_utf7
-
-server = "imap.gmail.com"
-imap_client = imaplib.IMAP4_SSL(server)
-result, data = imap_client.login("admin@hermancorp.biz", "DavidPrintedA3DThing!")
-print(result, data)
-result, data = imap_client.select('"{}"'.format(imap_utf7.encode("inbox").decode()), True)
-print(result, data[0])
